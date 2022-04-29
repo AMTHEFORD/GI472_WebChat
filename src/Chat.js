e6704985859b2d163a93cf68279c7edc6c3df00d
FORDz <thefordz.th@gmail.com>
2022-04-29 20:44:30 +0700

    update

---
+++ src/Chat.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import App from './components/App.js';
+import Chatbox from './components/Chatbox.js';
+import Titlebar from './components/Titlebar.js';
+import Inputbox from './components/Inputbox.js';
+import './Chat.css';
+import FriendList from './FriendList.js';
+import { useChatStateContext } from './context/FirebaseChatContextProvider';
+import { useAuthStateContext } from './context/FirebaseAuthContextProvider';
+import Tabbar from './components/Tabbar.js';
+import Nav from './components/Nav.js';
+
+function Chat() {
+  const { sendMsg, listenToChatroom, imageDict, userDict } = useChatStateContext();
+  const { authState } = useAuthStateContext();
+
+  const [chatData, setChatData] = useState([]);
+
+  function onMsg(msg) {
+    setChatData(oldChat => [msg, ...oldChat]);
+  }
+
+  function transformChatData(item) {
+    let displayName = "ไม่ทราบชื่อ(Offline)";
+    if (item.user in userDict && userDict[item.user].displayName){
+      displayName = userDict[item.user].displayName;
+    } else if (item.user === authState.user.uid) {
+      displayName = authState.user.displayName;
+    }
+
+    return ({
+      ...item,
+      key: item.timestamp || Date.now(),
+      displayName: displayName,
+      image: imageDict[item.user] || "",
+      position: item.user === authState.user.uid ? "right" : "left"
+    });
+  }
+
+  useEffect(() => {
+    const unsubscribeChatroom = listenToChatroom('mainhall', onMsg);
+
+    return () => {
+      unsubscribeChatroom();
+    }
+  }, [listenToChatroom]);
+
+
+  return (
+    <App>
+      <div className="chat">
+        <Titlebar value="Group Chat"/>        
+        <Chatbox data={chatData.map(transformChatData).sort((a,b)=>a.timestamp - b.timestamp)}/>
+        <Inputbox onEnter={x=>sendMsg(x,"mainhall")}/>
+      </div>
+      <Nav/>
+    </App>
+  );
+}
+
+export default Chat;
